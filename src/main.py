045399a8a256f60417bcadbfd390573e933b4c2c
luianqi <1ayanabdrahmanova1@gmail.com>
2022-10-11 00:27:37 +0600

    remove load_dotenv and middleware

--- src/main.py
+++ src/main.py
@@ -1,15 +1,8 @@
-import os
-
-from dotenv import load_dotenv
-from fastapi_sqlalchemy import DBSessionMiddleware
 from fastapi import FastAPI
 
 from src import healthcheck
 
-load_dotenv(".env")
 
 app = FastAPI()
-
-app.add_middleware(DBSessionMiddleware, db_url=os.environ["DATABASE_URL"].replace("postgres://", "postgresql://", 1))
 
 app.include_router(healthcheck.router)
