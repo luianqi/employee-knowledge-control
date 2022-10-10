27e3f4a1862f230d708a08accf71a0e8518f7b0e
luianqi <1ayanabdrahmanova1@gmail.com>
2022-10-10 19:52:39 +0600

    add health check router

--- src/main.py
+++ src/main.py
@@ -1,13 +1,15 @@
+import os
+
+from dotenv import load_dotenv
+from fastapi_sqlalchemy import DBSessionMiddleware
 from fastapi import FastAPI
+
+from src import healthcheck
+
+load_dotenv(".env")
 
 app = FastAPI()
 
+app.add_middleware(DBSessionMiddleware, db_url=os.environ["DATABASE_URL"].replace("postgres://", "postgresql://", 1))
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+app.include_router(healthcheck.router)
