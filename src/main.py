c5d7cedd46c356cf6d6b6fbfe9f62f5b427c4743
luianqi <1ayanabdrahmanova1@gmail.com>
2022-10-12 21:54:42 +0600

    add corsmiddleware

--- src/main.py
+++ src/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from src import healthcheck
-
 
 app = FastAPI()
 
 app.include_router(healthcheck.router)
+
+# cors
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
