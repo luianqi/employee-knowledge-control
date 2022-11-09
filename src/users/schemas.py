5a0952b18fa93ce97306d45cfd01bfd2627641df
Aiana Abdyrakhmanova <73655002+luianqi@users.noreply.github.com>
2022-11-10 03:45:10 +0600

    add roles enum and token model

--- src/users/schemas.py
+++ src/users/schemas.py
@@ -1,12 +1,21 @@
-from typing import Optional, List
+from enum import Enum
+from typing import Optional
 
 from pydantic import BaseModel, EmailStr
 
 
+class Roles(Enum):
+    user = "user"
+    owner = "owner"
+    admin = "admin"
+
+
 class UserBase(BaseModel):
+    id: int
     first_name: str
     last_name: str
     email: EmailStr
+    role: Roles = "user"
     is_active: Optional[bool] = True
 
     class Config:
@@ -26,7 +35,13 @@ class UserUpdate(BaseModel):
     first_name: Optional[str] = None
     last_name: Optional[str] = None
     email: Optional[EmailStr] = None
+    role: Optional[Roles] = None
     is_active: Optional[bool] = True
+
+
+class UpdatePassword(BaseModel):
+    current_password: Optional[str] = None
+    new_password: Optional[str] = None
 
 
 class UserInDBBase(UserBase):
@@ -41,5 +56,10 @@ class UserResponse(BaseModel):
     first_name: str
     last_name: str
     email: EmailStr
+
+
+class TokenData(BaseModel):
+    email: Optional[str] = None
+
 
 
