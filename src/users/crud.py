f14151479d59acff6808e9eb98bf986abcc2304f
Aiana Abdyrakhmanova <73655002+luianqi@users.noreply.github.com>
2022-11-10 03:43:22 +0600

    add role value in SignUp

--- src/users/crud.py
+++ src/users/crud.py
@@ -1,35 +1,34 @@
-from typing import Optional
-
 from fastapi import HTTPException
 
 from src.database import database
 from src.users import schemas
 from src.users.models import users
-from src.users.services import Hash
+from src.users.utils import Hash
 
 
-async def signUp(request: schemas.UserSignUp) -> Optional[users]:
-        """
+async def signUp(request: schemas.UserSignUp):
+    """
         Method for user to sign up
 
         :param request: UserSignUp schema, post body
         :return: created user
 
         """
-        query = users.insert().values(
-            first_name=request.first_name,
-            last_name=request.last_name,
-            email=request.email,
-            password=Hash.bcrypt(request.password),
-        )
+    query = users.insert().values(
+        first_name=request.first_name,
+        last_name=request.last_name,
+        email=request.email,
+        password=Hash.bcrypt(request.password),
+        role=request.role.value,
+    )
 
-        user_id = await database.execute(query)
-        return {**request.dict(), "id": user_id}
+    user_id = await database.execute(query)
+    return {**request.dict(), "id": user_id}
 
 
-async def updateUser(id: int, request: schemas.UserUpdate) -> Optional[users]:
+async def updateUser(id: int, request: schemas.UserUpdate):
     """
-    Method for updating the existing user
+    General method for updating the existing user, meaning only superuser can access it
 
     :param id: id of user
     :param request: UserUpdate schema, update body
@@ -39,7 +38,8 @@ async def updateUser(id: int, request: schemas.UserUpdate) -> Optional[users]:
     query = users.update().where(users.c.id == id).values(
         first_name=request.first_name,
         last_name=request.last_name,
-        email=request.email,)
+        role=request.role.value,
+        email=request.email, )
 
     await database.execute(query)
     return {**request.dict(), "id": id}
@@ -58,7 +58,7 @@ async def deleteUser(id: int):
                          detail="User with id: {} deleted successfully!".format(id))
 
 
-async def usersList(skip: int = 0, take: int = 100) -> Optional[users]:
+async def usersList(skip: int = 0, take: int = 100):
     """
     Method for listing all users
 
@@ -71,7 +71,7 @@ async def usersList(skip: int = 0, take: int = 100) -> Optional[users]:
         users.select().offset(skip).limit(take))
 
 
-async def get_user_by_email(email: str) -> Optional[users]:
+async def get_user_by_email(email: str):
     """
     Method for getting user by his email
 
@@ -84,7 +84,7 @@ async def get_user_by_email(email: str) -> Optional[users]:
     )
 
 
-async def get_user_by_id(id: int) -> Optional[users]:
+async def get_user_by_id(id: int):
     """
     Method for getting user by his id
 
@@ -95,3 +95,16 @@ async def get_user_by_id(id: int) -> Optional[users]:
     return await database.fetch_one(
         users.select().where(users.c.id == id)
     )
+
+
+# def update_password(db: Session, instance: models.User, data: schemas.ChangePassword):
+#     if instance.verify_password(data.current_password):
+#         instance.set_password(data.new_password)
+#         return True
+#     return False
+
+# async def UpdatePassword(request: schemas.UpdatePassword):
+#     if Hash.verify(request.current_password):
+#         Hash.bcrypt(request.new_password)
+#         return True
+#     return False
